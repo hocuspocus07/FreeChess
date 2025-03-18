9479f667c80059a959a8dc67e17c52cef8911997
hocuspocus07 <150130202+hocuspocus07@users.noreply.github.com>
2025-03-19 00:25:05 +0530

    modified middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,26 +1,34 @@
-import User from "../models/User.models.js";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+import User from '../models/User.models.js';
 
-dotenv.config({
-    path:"../../.env",
-})
-
-export const verifyJWT = async(req, res, next) => {
+dotenv.config()
+export const verifyJWT = async (req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-    if (!token) {
-        res.status(401).json({ message: 'UNAUTHORIZED REQUEST' });
-    }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    
-    const user = await User.findById(decodedToken._id);
-    if(!user){
-        res.status(401).json({message:'INVALID ACCESS TOKEN'});
-    }
-    req.user=user;
-    next();
+        const token = req.cookies.accessToken || req.header('Authorization')?.replace('Bearer ', '');
+        console.log('Token:', token); 
+        if (!token) {
+            return res.status(401).json({ message: 'UNAUTHORIZED REQUEST' });
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        console.log('Decoded Token:', decodedToken);
+
+        const user = await User.findById(decodedToken._id);
+        if (!user) {
+            return res.status(401).json({ message: 'INVALID ACCESS TOKEN' });
+        }
+
+        req.user = user;
+        next();
     } catch (error) {
-        res.status(500).json({message:'UNEXPECTED ERROR OCCURRED, ',error:error.message});
+        console.error('JWT Verification Error:', error);
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'TOKEN EXPIRED' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: 'INVALID TOKEN' });
+        } else {
+            return res.status(500).json({ message: 'UNEXPECTED ERROR OCCURRED', error: error.message });
+        }
     }
-}
+};
