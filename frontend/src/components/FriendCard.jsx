a6295cd5159061534b07e604e3f370cc4adc6a47
hocuspocus07 <150130202+hocuspocus07@users.noreply.github.com>
2025-06-10 01:15:59 +0530

    style: Remove dark style

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 function FriendCard({ friend, showRemove, onRemove,onClick }) {
   return (
-    <div onClick={() => onClick && onClick(friend.id)} className="bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden border border-gray-200 dark:border-gray-700 transition-all hover:shadow-lg">
+    <div onClick={() => onClick && onClick(friend.id)} className="bg-white rounded-xl shadow-md overflow-hidden border border-gray-200 transition-all hover:shadow-lg">
       <div className="flex flex-col items-center p-6">
         <div className="relative mb-4">
           <img
-            className="w-24 h-24 rounded-full object-cover shadow-lg border-2 border-lime-400 dark:border-lime-500"
+            className="w-24 h-24 rounded-full object-cover shadow-lg border-2 border-lime-400 "
             src={friend.profilePic || "/default-user.png"}
             alt={friend.username}
           />
         </div>
-        <h5 className="mb-1 text-xl font-semibold text-gray-900 dark:text-white">{friend.username}</h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400 mb-4">{friend.email}</span>
+        <h5 className="mb-1 text-xl font-semibold text-gray-900">{friend.username}</h5>
+        <span className="text-sm text-gray-500 mb-4">{friend.email}</span>
         {showRemove && (
           <button
             className="mt-2 px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 text-sm"
--- frontend/src/components/PopUp.jsx
+++ frontend/src/components/PopUp.jsx
@@ -10,12 +10,12 @@ function PopUp({ searchResults = [], onClose, onUserClick }) {
                 className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50"
             >
                 <div className="relative p-4 w-full max-w-md max-h-full m-auto mt-20">
-                    <div className="relative bg-[#2c2c2c] rounded-lg shadow-sm dark:bg-gray-700 text-[#7fa650]">
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-                            <h3 className="text-lg font-semibold text-white dark:text-white">Search Results</h3>
+                    <div className="relative bg-[#2c2c2c] rounded-lg shadow-sm text-[#7fa650]">
+                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+                            <h3 className="text-lg font-semibold text-white">Search Results</h3>
                             <button
                                 type="button"
-                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:cursor-pointer rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:cursor-pointer rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center"
                                 onClick={onClose}
                             >
                                 <svg
@@ -54,7 +54,7 @@ function PopUp({ searchResults = [], onClose, onUserClick }) {
                                 )}
                             </ul>
                             <button
-                                className="text-white inline-flex w-full justify-center hover:cursor-pointer bg-[#538f0b] hover:bg-[#506338] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                className="text-white inline-flex w-full justify-center hover:cursor-pointer bg-[#538f0b] hover:bg-[#506338] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                                 onClick={onClose}
                             >
                                 Close
